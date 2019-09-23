be878828bfcf424161e3c1be17f670ce3b575c1b
mookiie <mookiie.343@gmail.com>
2019-09-23 10:42:23 +0700

    upload (s)

--- api/v1/audio.js
+++ api/v1/audio.js
@@ -12,21 +12,28 @@ var urlencodedParser = bodyParser.urlencoded({
   limit: '10mb'
 });
 var multer = require('multer');
-var upload = multer({dest:'uploads/'});
+var upload = multer({
+  dest: 'uploads/'
+});
 
-router.post("/", jwt.verify,urlencodedParser, function (req, res, next){
+router.post("/", jwt.verify, urlencodedParser, function (req, res, next) {
   let { voice_name, voice_description, audio } = req.body
   let voice_id = uuidv1()
   let created_by = req.uuid;
   let created_dt = moment.utc().format('YYYY-MM-DD HH:mm:ss');
   let sql = `INSERT INTO voice_config (voice_id, voice_name, voice_description, voice_storage, created_dt, created_by)
             VALUES ('${voice_id}', '${voice_name}', '${voice_description}', '${audio}', '${created_dt}', '${created_by}')`
-  ivr.query(sql,function (response) {
+  ivr.query(sql, function (response) {
     res.status(201).json()
   })
 })
 
-router.post("/upload",jwt.verify,upload.single('audio'),function (req, res, next) {
-  console.log("/audio/upload",req);
+router.post("/upload", jwt.verify, upload.single('audio'), function (req, res, next) {
+  let file = req.file
+  console.log(file);
+  let uploadLocation = 'uploads/' + file.originalname // where to save the file to. make sure the incoming name has a .wav extension
+  fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(file.buffer)));// write the blob to the server as a file  
+  fs.unlink('uploads/' + file.filename, function (err) {})
+  res.status(200).json()
 })
 module.exports = router;
